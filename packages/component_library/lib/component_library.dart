54c525c91f30545881b68e8182e660fe8aedd799
Fabian Marti <fabian.marti@ceff.ch>
2025-02-07 15:27:10 +0100

    Component for the questions and I began creating a Answer button widget

--- packages/component_library/lib/component_library.dart
+++ packages/component_library/lib/component_library.dart
@@ -5,3 +5,5 @@ export 'src/tag.dart';
 export 'src/detail_quiz_item.dart';
 export 'src/start_button.dart';
 export 'src/detail_quiz_page.dart';
+export 'src/question_quiz.dart';
+export 'src/answer_item.dart';
