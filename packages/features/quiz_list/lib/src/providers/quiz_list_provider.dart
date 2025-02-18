a790cf98090d511fcdaca189a97b46036feb0a26
Fabian Marti <fabian.marti@ceff.ch>
2025-02-18 16:05:04 +0100

    The search is not sensible to diacritics

--- packages/features/quiz_list/lib/src/providers/quiz_list_provider.dart
+++ packages/features/quiz_list/lib/src/providers/quiz_list_provider.dart
@@ -1,3 +1,4 @@
+import 'package:diacritic/diacritic.dart';
 import 'package:domain_entities/domain_entities.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,13 +7,11 @@ import 'package:quiz_repository/quiz_repository.dart';
 part 'quiz_list_state.dart';
 
 class QuizListProvider with ChangeNotifier {
-  QuizListState _filterState = QuizListState.initial();
   QuizListState _state = QuizListState.initial();
 
   QuizListProvider({required this.repository});
   final QuizRepository repository;
 
-  QuizListState get filterState => _filterState;
   QuizListState get state => _state;
 
   Future<void> fetchAndSetQuizes() async {
@@ -37,17 +36,23 @@ class QuizListProvider with ChangeNotifier {
   void filterQuizzes(String value) {
     List<Quiz> quizzes = <Quiz>[];
 
+    final cleanValue = removeDiacritics(value.toLowerCase());
+
     if (value.isNotEmpty) {
       for (Quiz quiz in _state.quizzes) {
         if (value[0] == "#") {
           for (String tag in quiz.tags) {
-            if (tag.toLowerCase().contains(value.substring(1).toLowerCase())) {
+            final cleanTag = removeDiacritics(tag.toLowerCase());
+
+            if (cleanTag.contains(cleanValue.substring(1))) {
               quizzes.add(quiz);
               break;
             }
           }
         } else {
-          if (quiz.title.toLowerCase().contains(value.toLowerCase())) {
+          final cleanTitle = removeDiacritics(quiz.title.toLowerCase());
+
+          if (cleanTitle.contains(cleanValue)) {
             quizzes.add(quiz);
           }
         }
