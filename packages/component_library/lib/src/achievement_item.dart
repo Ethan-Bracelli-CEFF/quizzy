daf84cf7b7e7836d4358777c3d471f56c5df0089
Fabian Marti <fabian.marti@ceff.ch>
2025-02-25 15:04:58 +0100

    Added a componnent for the achievment, quiz list item now have an icon for the user achievment for the quiz shown

---
+++ packages/component_library/lib/src/achievement_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AchievementItem extends StatelessWidget {
+  const AchievementItem({required this.note, size, super.key})
+      : size = size ?? 32.0;
+
+  factory AchievementItem.fromPercentage(percentage, {size}) {
+    int note = 0;
+
+    while (percentage > 0) {
+      note++;
+      percentage -= 0.2;
+    }
+
+    return AchievementItem(note: note, size: size);
+  }
+
+  final int note;
+  final double size;
+
+  Color getNoteColor(int note) {
+    switch (note) {
+      case 5:
+        return Colors.yellow;
+      case 4:
+        return Colors.green;
+      case 3:
+        return Colors.yellow;
+      case 2:
+        return Colors.grey;
+      case 1:
+        return Colors.brown;
+      default:
+        return Colors.black38;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      note >= 5 ? Icons.emoji_events : Icons.star,
+      size: size,
+      color: getNoteColor(note),
+      shadows: [
+        note > 0
+            ? Shadow(
+                color: Colors.black87, offset: Offset(0, 1.5), blurRadius: 1.5)
+            : Shadow()
+      ],
+    );
+  }
+}
